2ad1f38ba1743cbc38f403714cbd46a41da1877c
Vương Viết Hoàng <hoangvv10@viettel.com.vn>
2020-04-22 18:10:01 +0700

    Temp commit

--- users/shadowprogr/rules.mk
+++ users/shadowprogr/rules.mk
@@ -1,2 +1,10 @@
 SRC += shadowprogr.c \
        process_records.c
+
+ifeq ($(strip $(OLED_DRIVER_ENABLE)), yes)
+    SRC += oled.c
+endif
+
+ifeq ($(strip $(RGBLIGHT_ENABLE)), yes)
+    SRC += rgb_light.c
+endif
