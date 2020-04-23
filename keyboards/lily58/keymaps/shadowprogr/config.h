c9673737413a7979e6998c8856b02c005de5cfce
Vương Viết Hoàng <hoangvv10@viettel.com.vn>
2020-04-23 18:08:45 +0700

    RGB matrix stuff

--- keyboards/lily58/keymaps/shadowprogr/config.h
+++ keyboards/lily58/keymaps/shadowprogr/config.h
@@ -1,26 +1,32 @@
 #pragma once
 
-/* Select hand configuration */
-
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
 #define SSD1306OLED
-
 #define USE_SERIAL_PD2
-
-#ifdef TAPPING_TERM
-  #undef TAPPING_TERM
-  #define TAPPING_TERM 200
-#endif
 
 #ifdef RGBLIGHT_ENABLE
   #undef RGBLED_NUM
-  #define RGBLIGHT_ANIMATIONS
   #define RGBLED_NUM 35
-  #define RGBLIGHT_HUE_STEP 8
-  #define RGBLIGHT_SAT_STEP 8
-  #define RGBLIGHT_VAL_STEP 5
-  #define RGBLIGHT_LIMIT_VAL 120
+  #define RGBLIGHT_ANIMATIONS
 #endif
+
+#ifdef RGB_MATRIX_ENABLE
+  #define DRIVER_LED_TOTAL 70
+  #define RGB_MATRIX_KEYPRESSES
+#endif
+
+// Keyboard info
+#undef VENDOR_ID
+#undef PRODUCT_ID
+#undef MANUFACTURER
+#undef PRODUCT
+#undef DESCRIPTION
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x1337
+#define DEVICE_VER      0x0100
+#define MANUFACTURER    Ape
+#define PRODUCT         ShadowProgr Lily58 Keyboard
+#define DESCRIPTION     Ergonomic split keyboard
