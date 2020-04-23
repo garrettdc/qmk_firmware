fdd1a4835b7759601250e9c4f9d6ed521bd7108a
Vương Viết Hoàng <hoangvv10@viettel.com.vn>
2020-04-23 18:08:45 +0700

    RGB matrix stuff

---
+++ users/shadowprogr/config.h
@@ -0,0 +1,22 @@
+// Set default TAPPING_TERM for all keyboards
+#ifdef TAPPING_TERM
+  #undef TAPPING_TERM
+  #define TAPPING_TERM 200
+#endif
+
+// Set default step and limit for all keyboards with RGB underlight
+#ifdef RGBLIGHT_ENABLE
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 5
+  #define RGBLIGHT_LIMIT_VAL 120
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+  #define RGB_MATRIX_HUE_STEP 8
+  #define RGB_MATRIX_SAT_STEP 8
+  #define RGB_MATRIX_VAL_STEP 5
+  #define RGB_MATRIX_SPD_STEP 10
+  #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 15
+  #define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
+#endif
--- users/shadowprogr/rules.mk
+++ users/shadowprogr/rules.mk
@@ -1,3 +1,37 @@
+# Enable Bootmagic Lite to consistently reset to bootloader and clear EEPROM.
+BOOTMAGIC_ENABLE = lite
+
+# Enable audio control and system control on all keyboards.
+EXTRAKEY_ENABLE = yes
+
+# Enable link-time optimization to reduce binary size.
+LINK_TIME_OPTIMIZATION_ENABLE = no
+
+# Disable unused build options on all keyboards.
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+MOUSEKEY_ENABLE = no
+NKRO_ENABLE = no
+TERMINAL_ENABLE = no
+
+# Disable unused hardware options on all keyboards.
+FAUXCLICKY_ENABLE = no
+MIDI_ENABLE = no
+SLEEP_LED_ENABLE = no
+
+# Disable other unused options.
+API_SYSEX_ENABLE = no
+AUTO_SHIFT_ENABLE = no
+COMBO_ENABLE = no
+KEYBOARD_LOCK_ENABLE = no
+KEY_LOCK_ENABLE = no
+LEADER_ENABLE = no
+SWAP_HANDS_ENABLE = no
+TAP_DANCE_ENABLE = no
+UCIS_ENABLE = no
+UNICODEMAP_ENABLE = no
+UNICODE_ENABLE = no
+
 SRC += shadowprogr.c \
        process_records.c
 
