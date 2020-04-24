c484934fd5976a50b517fd087b0a81345900d13e
ShadowProgr <shadowprogr@gmail.com>
2020-04-24 20:20:13 +0700

    Consistent styling

--- users/shadowprogr/process_records.c
+++ users/shadowprogr/process_records.c
@@ -1,32 +1,39 @@
 #include "shadowprogr.h"
 
+
+
+/* Allows usage of _keymap funtions in keymaps since _user funtions are used here. */
 __attribute__((weak))
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) { return true; }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case KC_P00:
-      if (record->event.pressed) {
-        SEND_STRING(SS_TAP(X_KP_0) SS_DELAY(5) SS_TAP(X_KP_0));
-      }
-      break;
 
-    // Layers
-    case QWERTY:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      break;
-    case DVORAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_DVORAK);
-      }
-      break;
-    case COLEMAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
-      }
-      break;
-  }
-  return process_record_keymap(keycode, record);
+
+/* Global, userspace functions that apply to all my keymaps */
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        /* Macros */
+        case KC_P00: // Send 2 KC_P0 in a row
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_KP_0) SS_DELAY(5) SS_TAP(X_KP_0));
+            }
+            break;
+
+        /* Layers */
+        case QWERTY:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_QWERTY);
+            }
+            break;
+        case DVORAK:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_DVORAK);
+            }
+            break;
+        case COLEMAK:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_COLEMAK);
+            }
+            break;
+        }
+    return process_record_keymap(keycode, record);
 }
--- users/shadowprogr/shadowprogr.c
+++ users/shadowprogr/shadowprogr.c
@@ -2,8 +2,7 @@
 
 
 
-// Allows usage of _keymap funtions in keymaps since _user
-// funtions are used here.
+/* Allows usage of _keymap funtions in keymaps since _user funtions are used here. */
 __attribute__((weak))
 void keyboard_pre_init_keymap(void) {}
 
@@ -18,23 +17,25 @@ layer_state_t layer_state_set_keymap(layer_state_t state) { return state; }
 
 
 
-// Global, userspace functions
+/* Global, userspace functions that apply to all my keymaps */
 void keyboard_pre_init_user(void) {
-  keyboard_pre_init_keymap();
+    keyboard_pre_init_keymap();
 }
 
 void matrix_init_user(void) {
-  matrix_init_keymap();
+    matrix_init_keymap();
 }
 
 void keyboard_post_init_user() {
-  keyboard_post_init_keymap();
+    keyboard_post_init_keymap();
 }
 
 layer_state_t layer_state_set_user (layer_state_t state) {
-  state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-#if defined(RGBLIGHT_ENABLE)
-  state = layer_state_set_rgb_light(state);
-#endif  // RGBLIGHT_ENABLE
-  return layer_state_set_keymap(state);
+    state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+
+    #if defined(RGBLIGHT_ENABLE)
+    state = layer_state_set_rgb_light(state);
+    #endif
+
+    return layer_state_set_keymap(state);
 }
