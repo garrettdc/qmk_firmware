a9935722f589baf16a2b70b5685cd44994e52c6b
ShadowProgr <shadowprogr@gmail.com>
2020-04-24 20:22:46 +0700

    More styling changes

--- keyboards/lily58/keymaps/shadowprogr/keymap.c
+++ keyboards/lily58/keymaps/shadowprogr/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
         CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_QUT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS,         KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-                                KC_LALT, KC_LGUI, KC_SPC,  LOWER,           RAISE,   KC_BSPC, KC_RGUI, NUMPAD                              \
+                                   KC_LALT, KC_LGUI, KC_SPC,  LOWER,           RAISE,   KC_BSPC, KC_RGUI, NUMPAD                              \
     ),
 
     /* DVORAK
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                              KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSLS, \
         CTL_ESC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                              KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    CTL_SLS, \
         KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_MINS,         KC_EQL,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, \
-                                KC_LALT, KC_LGUI, KC_SPC,  LOWER,           RAISE,   KC_BSPC, KC_RGUI, NUMPAD                              \
+                                   KC_LALT, KC_LGUI, KC_SPC,  LOWER,           RAISE,   KC_BSPC, KC_RGUI, NUMPAD                              \
     ),
 
     /* COLEMAK
